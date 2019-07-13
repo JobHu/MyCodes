03711c26ecdf3c1bd5c15adab0b0924dc22791f4
hyg <hyg@192.168.1.104>
2019-07-13 10:57:19 +0800

    第二次提交

--- MyFirstGwt/src/hello/HaHa.java
+++ MyFirstGwt/src/hello/HaHa.java
@@ -3,5 +3,9 @@ package hello;
 public class HaHa {
  public static void main(String[] args) {
 	System.out.println();
+	
+	String hello = "111111";
+	String b =hello.substring(0);
+	System.out.println(b);
 }
 }
